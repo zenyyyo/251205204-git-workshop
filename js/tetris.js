eb4a46578e874ccd9d9e1baee9bb06ff44f7a74d
zenyyyo <251205204@ccmailg.meijo-u.ac.jp>
2026-05-18 14:33:51 +0900

    色変えた

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'lime', 'purple'
+    'skyblue', 'orange', 'blue', 'yellow', 'red', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
